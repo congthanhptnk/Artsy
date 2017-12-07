b0f0f8a1fedc27e69f4740a134db3c11136ed6b5
Dat Truong <dat.truonghuu1212@gmail.com>
2017-12-07 21:10:10 +0200

    revert

---
+++ src/java/wService/CommentFacadeREST.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wService;
+
+import entities.Comment;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author congthanhptnk
+ */
+@Stateless
+@Path("comments")
+public class CommentFacadeREST extends AbstractFacade<Comment> {
+
+    @PersistenceContext(unitName = "ArtsyPU")
+    private EntityManager em;
+
+    public CommentFacadeREST() {
+        super(Comment.class);
+    }
+
+    @POST
+    @Path("{userid}/{postid}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public boolean createComment(
+            @PathParam("postid")int pid, 
+            @PathParam("userid")int id, 
+            @FormParam("comment")String comment) {
+        boolean isOk =true;
+        if(comment.isEmpty()){
+            isOk = false;
+        }
+        else {
+            Comment myComment = new Comment();
+            myComment.setPid(pid);
+            myComment.setId(id);
+            myComment.setComment(comment);
+            super.create(myComment);
+        }
+        
+        return isOk;
+    }
+
+    @DELETE
+    @Path("{userid}/{commentid}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public boolean removeComment(@PathParam("commentid")int cid, @PathParam("userid")int id) {
+        boolean isOk = false;
+        Comment oldComment = super.find(cid);
+        if(oldComment.getId() == id){
+            super.remove(super.find(id)); 
+            isOk = true;
+        }
+        return isOk;
+    }
+
+    @GET
+    @Path("{userid}/{postid}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Comment> findPicComment(@PathParam("postid")int pid) {
+        List<Comment> comments = em.createNamedQuery("Comment.findCommentbyPid").setParameter("PID", pid).getResultList();
+        return comments;
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
---
+++ src/java/wService/PostFacadeREST.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wService;
+
+import entities.Post;
+import entities.User;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+
+/**
+ *
+ * @author congthanhptnk
+ */
+@Stateless
+@Path("posts")
+public class PostFacadeREST extends AbstractFacade<Post> {
+
+    @PersistenceContext(unitName = "ArtsyPU")
+    private EntityManager em;
+
+    public PostFacadeREST() {
+        super(Post.class);
+    }
+
+    @POST
+    @Path("{userid}/newpost")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Post createPost(
+            String title, 
+            String caption,
+            String picture, 
+            int id) {
+        
+        Post newPost;
+        if(picture.isEmpty() || title.isEmpty()){
+            newPost = em.find(Post.class, 1);
+        }
+        else{
+            newPost = new Post();
+            newPost.setCaption(caption);
+            newPost.setPicture(picture);
+            newPost.setTitle(title);
+            newPost.setId(id);
+            super.create(newPost);
+        }
+        return newPost;
+        
+    }
+
+    @DELETE
+    @Path("{userid}/{postid}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public boolean removePost(@PathParam("postid")int pid, @PathParam("userid")int id) {
+        boolean isOk = true;
+        
+        if(super.find(pid)==null){
+            isOk = false;
+        }
+        super.remove(super.find(pid));
+        return isOk;
+    }
+
+    @GET
+    @Path("{userid}/main")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Post> findAllPost() {
+        return super.findAll();
+    }
+    
+    @GET
+    @Path("{userid}/mypost")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Post> findMyPost(@PathParam("userid")int id) {
+        List<Post> myPost = em.createNamedQuery("Post.findById").setParameter("ID", id).getResultList();
+        return myPost;
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
